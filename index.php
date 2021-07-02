a6be2217dad59a3a6068f87f2aa90c3cd62e191a
EliseAndaloro <elise.andaloro@hotmail.fr>
2021-07-02 14:59:40 +0200

    Mise en place avis clients

--- index.php
+++ index.php
@@ -14,9 +14,39 @@ ob_start();
 <div class="coaching-pers">
 	<div class="text-center">
 		<h2>Coaching<br>personnalisé</h2>
-		<div class="ligne"></div>
+		<div class="ligne text-center"></div>
 		<p>Atteignez vos objectifs plus rapidement<br>grâce à un suivi totalement personnalisé</p>
 		<a href="#" class="btn btn-primary">Choisir mon programme</a>
+	</div>
+</div>
+
+<div class="avis-clients">
+	<div class="container">
+		<div class="text">
+			<h2>Les clients disent</h2>
+			<div class="ligne"></div>
+			<div class="row text-center">
+				<div class="col">
+					<div>
+						<img src="./images/kw-coaching-header.jpg" alt="" width="200" height="400">
+						<p>lorem ipsum</p>
+					</div>
+				</div>
+				<div class="col">
+					<div>
+						<img src="./images/kw-coaching-header.jpg" alt="" width="200" height="400">
+						<p>lorem ipsum</p>
+					</div>
+				</div>
+				<div class="col">
+					<div>
+						<img src="./images/kw-coaching-header.jpg" alt="" width="200" height="400">
+						<p>lorem ipsum</p>
+					</div>
+				</div>
+			</div>
+			<p>Atteignez vos objectifs plus rapidement<br>grâce à un suivi totalement personnalisé</p>
+		</div>
 	</div>
 </div>
 
