5b064290b8a35bb3900bb6dad8237ec543f8d711
EliseAndaloro <elise.andaloro@hotmail.fr>
2021-07-13 12:03:59 +0200

    Création formulaire + affichage en fonction du type

--- formulaire_contact.php
+++ formulaire_contact.php
@@ -14,24 +14,18 @@ ob_start();
 					<form method="post">
 						<h2>Me contacter</h2>
 						<div class="ligne" style="margin-bottom:3%"></div>
-						<div class="form-group row" style="margin-bottom:3%">
-    						<label for="name" class="col-sm-3 col-form-label">Name</label>
+                        <div class="form-group row" style="margin-bottom:3%">
+    						<label for="type" class="col-sm-3 col-form-label">Vous êtes : </label>
 							    <div class="col-sm-9">
-							    	<input type="text" class="form-control" id="name" name="name">
+                                    <select class="form-control" name="type" id="type" onchange="helloWorld();">
+                                        <option value=""> Choisissez une option </option>
+                                        <option value="particulier"> Un Particulier </option>
+                                        <option value="entreprise"> Une Entreprise </option>
+                                    </select>
 							    </div>
   						</div>
-  						<div class="form-group row" style="margin-bottom:3%">
-    						<label for="staticEmail" class="col-sm-3 col-form-label">Email</label>
-    							<div class="col-sm-9">
-      								<input type="email" class="form-control" id="email" name="email">
-    							</div>
-  						</div>
-  						<div class="form-group row" style="margin-bottom:3%">
-    						<label for="description" class="col-sm-3 col-form-label">Description</label>
-    							<div class="col-sm-9">
-      								<input type="textarea" class="form-control" id="description" name="description">
-    							</div>
-  						</div>
+                        <div id="form_content">
+                        </div>
 						<div class="form-group text-right" style="text-align: end;">
 							<input type="submit" class="btn btn-outline-primary" name="envoyer" value="ENVOYER">
 						</div>
@@ -39,7 +33,7 @@ ob_start();
 				</div>
 			</div>
 		</div>
-
+<script src="./js/kw_inc_fonction_formulaire_contact.js"></script>
 <?php
 $content = ob_get_clean();
 require('template.php');
